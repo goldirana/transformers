774990b13d02f1dc466645192ca00a7524f41998
goldirana <goldirana3210@gmail.com>
2025-03-05 22:42:26 +0530

    .

--- src/config/configuration.py
+++ src/config/configuration.py
@@ -1,7 +1,7 @@
 from src.entity.config_entity import DistilBert
 import os
-
-model_path = os.path.join(os.getcwd(), "models")
+from pathlib import Path
+model_path = Path(os.path.join(os.getcwd(), "models"))
 
 class ConfigurationManager:
     def __init__(self):
@@ -9,9 +9,18 @@ class ConfigurationManager:
     
     def get_distilbert_config(self) -> DistilBert:
         params = DistilBert(
-            model_path=str(os.path.join(model_path,'/distilbert/model')),
-            tokenizer_path=str(os.path.join(model_path,'/distilbert/tokenizer')),
-            config_path=str(os.path.join(model_path,'/distilbert/config')),
+            model_path=str(Path(os.path.join(model_path,'distilbert/model'))),
+            tokenizer_path=str(Path(os.path.join(model_path,'distilbert/tokenizer'))),
+            config_path=str(Path(os.path.join(model_path,'distilbert/config'))),
             model_name='distilbert')
+        print(model_path)
         return params
     
+if __name__ == "__main__":
+    config_manager = ConfigurationManager()
+    params = config_manager.get_distilbert_config()
+    print(params.model_path)
+    print(params.tokenizer_path)
+    print(params.config_path)
+    print(params.model_name)   
+    print(model_path)
